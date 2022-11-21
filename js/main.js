39e73e4f7cc1a06868b07b2d87e426c49c87d55c
GiuseppeRepici <giuseppe.repici95@gmail.com>
2022-11-21 16:21:05 +0100

    completed + bonus

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const { createApp } = Vue;
+
+
+createApp({
+    
+    data () {
+      return {
+        classeh1: "titolo",
+        nome: "peppe",
+        cognome: "repici",
+        eta: "26",
+        foto: "img/facebook.jpg"
+      };
+    },
+  }).mount("#app")
